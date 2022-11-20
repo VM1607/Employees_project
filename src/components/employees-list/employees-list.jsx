c3bd572cdc1c32b6d6a1f96320635c51fafde8de
Vladislav Murzin <vlad99samara@mail.ru>
2022-11-20 22:01:51 +0400

    first commit

---
+++ src/components/employees-list/employees-list.jsx
@@ -0,0 +1,14 @@
+import EmployeersListItem from '../employees-list-item/employees-list-item';
+import './employees-list.css';
+
+const EmployeesList = () => {
+    return (
+        <ul className="app-list list-group">
+            <EmployeersListItem/>
+            <EmployeersListItem/>
+            <EmployeersListItem/>
+        </ul>
+    )
+}
+
+export default EmployeesList;
