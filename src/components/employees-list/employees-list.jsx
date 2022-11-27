7d4543702f3a7c7d12f9c753ea19ebcfac8b4353
Vladislav Murzin <vlad99samara@mail.ru>
2022-11-27 22:06:23 +0400

    commit

--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -1,12 +1,22 @@
 import EmployeersListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data, onDelete, onToggleProp}) => {
+
+    const elements = data.map(item => {
+        const {id, ...itemProps} = item;
+        return (
+            <EmployeersListItem 
+                key={id} 
+                {...itemProps}
+                onDelete={() => onDelete(id)}
+                onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}/>
+        )
+    })
+
     return (
         <ul className="app-list list-group">
-            <EmployeersListItem/>
-            <EmployeersListItem/>
-            <EmployeersListItem/>
+            {elements}
         </ul>
     )
 }
